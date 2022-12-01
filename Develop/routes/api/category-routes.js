b492e19c60868faaab3595ce998fb286950f76f3
sophiekedrowski <105030180+sophiekedrowski@users.noreply.github.com>
2022-12-01 13:12:25 -0600

    Finished all my API routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -21,9 +21,9 @@ router.get('/:id', async (req, res) => {
   //   find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findByPk(req.params.id, { 
-      include: Product 
-      });
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: Product
+    });
     if (!categoryData) {
       res.status(404).json({ message: 'Couldnt locate category' });
       return;
@@ -37,7 +37,14 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
@@ -62,8 +69,21 @@ router.put('/:id', async (req, res) => {
 }
 );
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!categoryData) {
+      res.status(404).json({ message:'no category found'});
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
